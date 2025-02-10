f4ccdaa933e3c25855a86a0501dbb791d06b4c24
grigoryan.andrew <grigoryan.andrew@gmail.com>
2025-02-10 16:11:36 +0100

    test commit

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
-Injectable({
+@Injectable({
   providedIn: 'root',
-});
+})
 export class MoviesService {
   private httpClient = inject(HttpClient);
 
